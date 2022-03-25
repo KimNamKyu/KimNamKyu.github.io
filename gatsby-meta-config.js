0d6eac75f1985a14a683257588c0f7abb7093df0
KimNamKyu <skarb629@gmail.com>
2022-03-25 20:11:02 +0900

    rename

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: `Blog posted about ...`,
   author: `[Your Name]`,
   introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://kimnamkyu.github.io/southkyu.io/`, // Your blog site url
+  siteUrl: `https://github.com/KimNamKyu/KimNamKyu.github.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: ``, // Your GitHub account
@@ -16,7 +16,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'KimNamKyu/southkyu.io', // Your repository for archive comment
+    utterances: 'KimNamKyu/KimNamKyu.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
